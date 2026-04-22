396f883e4c3296db6f0ad6812e7b51aa08da09e2
taehyeung123 <taehyeung123@users.noreply.github.com>
2026-04-22 15:27:12 +0900

    chore: remove temporary analysis scripts

--- get_dims.ps1
+++
@@ -1,6 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-Get-ChildItem "C:\Users\rnjsr\.gemini\antigravity\scratch\ace-wedding\images\mobile\*.jpg" | Sort-Object { [int]($_.BaseName -replace '\D','') } | ForEach-Object {
-    $img = [System.Drawing.Image]::FromFile($_.FullName)
-    Write-Output "$($_.Name): $($img.Width)x$($img.Height)"
-    $img.Dispose()
-}
